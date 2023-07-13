2a69502eead50ff2a2dbfe00d20d15accdc0b871
Winston <jdwinston@wpi.edu>
2023-07-13 10:26:42 -0400

    thursday morning

---
+++ dice/dice.js
@@ -0,0 +1,10 @@
+function roll() {
+    const sides = 6;
+    var randomnumber = Math.floor(Math.random()* (sides)+ 1);
+    let face = document.getElementById("face");
+    face.innerHTML = randomnumber;
+
+}
+
+var button = document.getElementById("button");
+button.addEventListener("click", roll);
