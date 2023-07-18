faa38343cdb18b355ae29381ae67b4560562a221
Winston <jdwinston@wpi.edu>
2023-07-18 16:24:57 -0400

    ping pong

--- robPong/Ball.js
+++ robPong/Ball.js
@@ -9,8 +9,8 @@ class Ball {
     }
     
     draw() {
-        ctx.fillStyle = "yellow";
-        ctx.strokeStyle = "black";
+        ctx.fillStyle = "black";
+        ctx.strokeStyle = "white";
         ctx.lineWidth = 2;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
@@ -20,41 +20,38 @@ class Ball {
     bounceWall() {
         //top wall
         if (this.y < this.r) {
-            this.vy = .9 * Math.abs(this.vy)
+            this.vy = 1.1 * Math.abs(this.vy);
         } 
+        //bottom wall
         if (this.y > BOARDHEIGHT - this.r) {
-            this.vy = -.9 * Math.abs(this.vy); 
+            this.vy = -1.1 * Math.abs(this.vy); 
         } 
     }
 
-    bouncePaddleL(paddle){
-        //not bouncing on paddle
+    bouncePaddleL(paddle) {
         if (this.x - this.r > paddle.w) return false;
-        //ball higher than paddle
-        if (this.y < paddle.y) return true;
-        //ball lower than paddle
-        if (this.y > paddle.y + paddle.l) return true;
-        //ball hit paddle
-        this.vx = PADDLEFORCE * Math.abs(this.vx)
-        let paddlePos = (this.y - paddle.y - paddle.l/2) / paddle.l * 2; //between -1.0 and 1.0
-
-        this.vy = this.vy + paddlePos*PADDLESPIN;
-
+        if (this.x - this.r < 0) return true;
+        if (this.y < paddle.y) return false;
+        if (this.y > paddle.y + paddle.l) return false;
+        if (this.vx < 0) {
+            this.vx = PADDLEFORCE * Math.abs(this.vx);
+            let paddlePos = (this.y - paddle.y - paddle.l/2) / paddle.l * 2; // between -1.0 and 1.0
+            this.vy = this.vy + paddlePos*PADDLESPIN;
+        }
         return false;
     }
-    bouncePaddleR(paddle){
-          //not bouncing on paddle
-          if (this.x + this.r < paddle.x) return false;
-          //ball higher than paddle
-          if (this.y < paddle.y) return true;
-          //ball lower than paddle
-          if (this.y > paddle.y + paddle.l) return true;
-          //ball hit paddle
-          this.vx = -PADDLEFORCE * Math.abs(this.vx)
-          let paddlePos = (this.y - paddle.y - paddle.l/2) / paddle.l * 2; //between -1.0 and 1.0
-  
-          this.vy = this.vy + paddlePos*PADDLESPIN;
-        
+
+    bouncePaddleR(paddle) {
+        if (this.x + this.r < paddle.x) return false;
+        if (this.x + this.r > paddle.x + paddle.w) return true;
+        if (this.y < paddle.y) return false;
+        if (this.y > paddle.y + paddle.l) return false;
+        if (this.vx > 0) {
+            this.vx = -PADDLEFORCE * Math.abs(this.vx);
+            let paddlePos = (this.y - paddle.y - paddle.l/2) / paddle.l * 2; // between -1.0 and 1.0
+            this.vy = this.vy + paddlePos*PADDLESPIN;
+        }
+        return false;
     }
 
 
--- robPong/pong.js
+++ robPong/pong.js
@@ -4,15 +4,32 @@ const ctx = gameboard.getContext("2d");
 const BOARDHEIGHT = 500;
 const BOARDWIDTH = 500;
 const PADDLESPIN = 1.5;
-const PADDLEFORCE = 1.1;
+const PADDLEFORCE = 1.4;
 
 let ball = new Ball(250,250,1,-4,12.5);
-let paddleL = new Paddle(0, 0, 100, 25, "red");
-let paddleR = new Paddle(475, 0 ,100, 25, "blue");
+let paddleL = new Paddle(0, 0, 250, 25, "red");
+let paddleR = new Paddle(475, 0 ,250, 25, "blue");
 
-function clearBoard() {
-    ctx.fillStyle = "grey";
+var grd = ctx.createLinearGradient(0, 0, 500, 0);
+grd.addColorStop(0, "tan");
+grd.addColorStop(1, "turquoise");
+
+function clearBoard() { 
+    ctx.fillStyle = grd;
     ctx.fillRect(0, 0, 500, 500);
+    ctx.strokeStyle = "yellow";
+    ctx.strokeRect(0, 0, 500, 500);
+    ctx.moveTo(BOARDWIDTH/2, 0)
+    ctx.lineTo(BOARDWIDTH/2,BOARDHEIGHT)
+    ctx.lineWidth = 3;
+    ctx.lineCap = "round";
+    ctx.stroke();
+}
+
+function updateScore() {
+    const scoreBoard = document.getElementById("scoreboard")
+    scoreBoard.innerHTML = `${scoreL} : ${scoreR}`;
+    callWinner();
 }
 
 function draw() {
@@ -31,24 +48,61 @@ function nextTick() {
             paddleR.move()
             
             ball.bounceWall()
-            ball.bouncePaddleL(paddleL);
-            ball.bouncePaddleR(paddleR);
+            
+            if (ball.bouncePaddleL(paddleL) || (ball.x < 0)) score("right");
+            
+            if (ball.bouncePaddleR(paddleR)) score("left");
             ball.move();
             
-
+            
             
             draw();
             nextTick();
         }, 10
         );
     }
-    function resetGame(){
-        clearInterval(intervalID);
-        draw();
-        nextTick();
+    
+    function score(player) {
+        if (player == "left") scoreL++;
+        if (player == "right") scoreR++;
+        
+        let vx = 1;
+        if (player == "right") vx = -2;
+        if (player == "left") vx = 2;
+        ball = new Ball(250, 250, vx, -1, 12.5);
+        updateScore();
     }
 
-
+    function callWinner() {
+        if (scoreL == 5) {
+            alert("Red wins!")
+            resetGame();
+        }
+        if (scoreR == 5) {
+            alert("Blue wins!")
+            resetGame();
+        }
+    }
+    
+    function resetGame(){
+        clearInterval(intervalID);
+        ball = new Ball(250, 250, 1, -1, 12.5);
+        paddleL = new Paddle(0, 0, 140, 25, "red");
+        paddleR = new Paddle(475, 0, 140, 25, "blue");
+        scoreL = 0;
+        scoreR = 0;
+        draw();
+        updateScore();
+        nextTick();
+    }
+    
+    function resetObjects() {
+        ball = new Ball(250, 250, 1, -1, 12.5);
+        paddleL = new Paddle(0, 0, 100, 25, "red");
+        paddleR = new Paddle(475, 0, 100, 25, "blue");
+    }
+    
+    
     const UPARROW = 38;
     const DOWNARROW = 40;
     const WKEY = 87;
@@ -80,7 +134,7 @@ function nextTick() {
         const key = event.keyCode;
         // console.log(' KEYUP:   ${Key}')
         console.log ('KEYDOWN: ${key}')
-
+        
         switch(key) {
             case (UPARROW):
             case (DOWNARROW):
